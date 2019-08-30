a8b565853f795ba56df483d437629a7959bb6578
Adrien CHAUVIN <adrienchauvin44@gmail.com>
2019-08-30 17:40:42 +0200

    Ajout des méthodes calculs trajet et durée trajet

--- src/main/java/dev/service/AnnonceCovoitService.java
+++ src/main/java/dev/service/AnnonceCovoitService.java
@@ -58,6 +58,7 @@ public class AnnonceCovoitService {
 
 	private static final int PLACE_MINIMUM_DISPONIBLE = 1;
 	private static final int PLACE_MAXIMUM_DISPONIBLE = 20;
+	private static final double VITESSE_MOYENNE_TRAJET = 70.0; 
 
 	public void verifierInfos(InfoCovoit infoCo, LocalDateTime dateTime, String email) throws MessagingException {
 		Map<String, String> erreurs = new HashMap<>();
@@ -187,6 +188,33 @@ public class AnnonceCovoitService {
 
 		javaMailSender.send(message);
 
+	}
+	
+	private static double calculDistanceTrajet(double lat1, double lon1, double lat2, double lon2, String unit) {
+		
+		if ((lat1 == lat2) && (lon1 == lon2)) {
+			return 0;
+		}
+		else {
+			double theta = lon1 - lon2;
+			double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+			dist = Math.acos(dist);
+			dist = Math.toDegrees(dist);
+			dist = dist * 60 * 1.1515;
+			if (unit == "K") {
+				dist = dist * 1.609344;
+			} else if (unit == "N") {
+				dist = dist * 0.8684;
+			}
+			return (dist);
+		}
+	}
+	
+	private static double calculDureeTrajet (double distanceTrajet) {
+		double dureeTrajet = distanceTrajet / VITESSE_MOYENNE_TRAJET;
+		long v1 = Math.round(dureeTrajet);
+		String date = v1 / 60 + "h" + v1%60;
+		return (dureeTrajet);
 	}
 
 }

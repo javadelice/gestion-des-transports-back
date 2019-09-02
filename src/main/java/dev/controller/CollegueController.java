0f755b55155cc5f97ae69b90d7299cda4c3cec94
Mathilde MUNIER <muniermathilde@gmail.com>
2019-09-02 15:07:42 +0200

    #17 optimisation du front

--- src/main/java/dev/controller/CollegueController.java
+++ src/main/java/dev/controller/CollegueController.java
@@ -1,22 +1,14 @@
 package dev.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import dev.domain.Collegue;
 import dev.controller.vm.ChauffeurVM;
-import dev.controller.vm.CollegueVM;
-import dev.domain.Role;
 import dev.dto.ChauffeurIdDTO;
 import dev.service.CollegueService;
 
